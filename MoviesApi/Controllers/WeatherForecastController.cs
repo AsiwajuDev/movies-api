a01fb4673c176b795a3af82472f4175e4ce6ca18
asiwaju07 <kazeem.asiwaju-bello@thepfs.biz>
2021-09-16 14:26:28 +0100

    Initial commit

---
+++ MoviesApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MoviesApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly InMemoryRepository _repo;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, InMemoryRepository repo)
+        {
+            _logger = logger;
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _repo.GetAllGenres();
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ MoviesApi/Services/InMemoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using MoviesApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi.Services
+{
+    public class InMemoryRepository : IRepository
+    {
+        private List<Genre> _genres;
+
+        public InMemoryRepository() 
+        {
+            _genres = new List<Genre>()
+            {
+                new Genre(){Id = 1, Name = "Comedy"},
+                new Genre(){Id = 2, Name = "Action"},
+                new Genre(){Id = 3, Name = "Thriller"}
+            };            
+        }
+
+        public List<Genre> GetAllGenres()
+        {
+            return _genres;
+        }
+    }
+}
